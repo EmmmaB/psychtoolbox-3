98ef4da4ec3d45dfb6f0e0476eca91a6060a4c3e
kleinerm <kleinerm@f5efbe48-b203-0410-9429-fc5e098e5a54>
2005-10-28 02:13:34 +0000

    release-1.0.6 removed.

--- release-1.0.6/Psychtoolbox/PsychTests/TestTextInitBug.m
+++
@@ -1,9 +0,0 @@
-% Tests for a bug in DrawText where failing to initialize text paramters
-% causes a SCREEN crash
-
-
-s=max(Screen('Screens'));
-w=Screen('OpenWindow', s, [], [], [], 2);
-Screen('DrawText', w, 'Hello!');
-Screen('Flip', w);
-
