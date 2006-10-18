09a526d4e6cfe16ae41f57f568a9f276e27a4cfd
kleinerm <kleinerm@f5efbe48-b203-0410-9429-fc5e098e5a54>
2006-10-18 01:31:38 +0000

    STABLE BRANCH UPDATED TO BETA REVISION 509 via svn merge -r 483:HEAD beta/ trunk/
    * More GetChar improvements.
    * Some small bug fixes and new functions.
    * Cleanup work by Chris and David.
    
    -> Stable is identical to beta except for the PR-650 toolbox and
       the IntelMac-Mexfiles.

--- stable/Psychtoolbox/PsychDemos/Contents.m
+++ stable/Psychtoolbox/PsychDemos/Contents.m
@@ -3,34 +3,40 @@
 % help Psychtoolbox     % For an overview, triple-click me & hit enter.
 % help PsychDemos       % For demos, triple-click me & hit enter.
 %
-%    AlphaImageDemoOSX       - Simple demo of OpenGL alpha blending.
-%    AlphaRotateDemoOSX      - Looks like a variant of AlphaImageDemoOSX
-%    ArcDemoOSX              -
+%    AlphaImageDemo          - Simple demo of OpenGL alpha blending.
+%    AlphaRotateDemoOSX      - A variant of AlphaImageDemoOSX, demonstrates rotated drawing.
+%    ArcDemoOSX              - Demonstrate the 'FrameArc', 'FillArc' functions.
 %    BubbleDemoOSX           - Like GazeContingentDemoOSX, but with multiple apertures.
 %    CLUTAnimDemoOSX         - Demonstrate lookup table animation.
 %    DotDemo                 - Animate dots with Screen('DrawDots).
-%    DKLDemo				 - Demonstrate DKL color space.
-%    DrawMirroredTextOSX     - 
+%    DKLDemo                 - Demonstrate DKL color space.
+%    DrawFormattedTextDemo   - Demonstrate formatted text drawing via DrawFormattedText()
+%    DrawMirroredTextOSX     - Shows how to draw horizontally mirrored text.
 %    DrawSomeTextOSX         - Very simple example of using 'DrawText'.
 %    DriftDemoOSX            - Animation of grating using textures.
 %    DriftDemoOSX2           - More efficient animation of gratings using one texture.
 %    DriftDemoOSX3           - Even more efficient animation of gratings.
 %    DriftWaitDemoOSX        - Show how to optimally time animations.
+%    ErrorCatchDemo          - How to handle errors gracefully (try/catch).
 %    FitGammaDemo            - Demonstrate gamma fitting routine FitGamma.
+%    FontDemo                - How to check for font availability and set font.
 %    GazeContingentDemoOSX   - Implementation of a simple gaze contingent display 
 %    GratingDemo             - Basic display of a grating on the screen.
 %    IsomerizationsInDishDemo - Compute photoreceptor isomerizations for retina in a dish.
 %    IsomerizationsInEyeDemo  - Compute photoreceptor isomerizations for human eye.
+%    KbDemo                  - Demonstrate KbCheck, KbName, KbWait.
 %    LineStippleDemoOSX      - Draw dotted and dashed lines.  
 %    MouseTraceDemoOSX       - Track mouse around screen and draw contour.
 %    MouseTraceDemo2OSX      - More Tracking mouse around screen drawing contour.
 %    MovieDemoOSX            - Animation without offscreen windows.
 %    NomogramDemo            - Compute photoreceptor nomograms.
+%    OldNewRecogDemo         - A full old-new recognition experiment.
+%    OpenGL4MatlabDemos      - Subfolder: Demos on how to use OpenGL4Matlab functions.
+%    QuicktimeDemos          - Subfolder: Demos on how to use Quicktime movie playback.
 %    StereoDemo              - Drive stereo-displays e.g., CrystalEyes.
 %    SpriteDemo              - Animates an image that follows the mouse.
-%    SoundTrigger            -
+%    SoundTrigger            - Demo of a very simple sound based trigger.
 %    StoreBit                - Not at all clear why this is here
-%    StressQuickTimeOSX      - Why isn't this in PsychTests?
 %    ValetonVanNorrenDemo    - Exercise our code that implements the V-VN (1983) cone adaptation model.
-%    VideoCaptureDemoOSX     -
+
 
